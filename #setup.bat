a7e89ce26e431e3d018f591d793a044eb9cc1412
Tgirltokyo <pkaciemalone@gmail.com>
2026-08-30 14:39:44 +0100

    Initial Release

---
+++ #setup.bat
@@ -0,0 +1,15 @@
+@echo off
+
+python -m venv .venv
+
+call .venv\Scripts\activate
+
+python -m pip install --upgrade pip
+
+pip install -r requirements.txt
+
+playwright install firefox
+
+python main.py
+
+pause
